0dde4dba91a412149d2776d55d248af9fd104823
Tis-Control-HA <tishomeassistant@gmail.com>
2025-10-19 17:07:09 +0300

    new version

--- climate.py
+++ climate.py
@@ -6,9 +6,12 @@ _G=alpha__("bnVtYmVy")
 _F=alpha__("ZmVlZGJhY2tfdHlwZQ==")
 _E=False
 _D=True
-_C=alpha__("bWlu")
-_B=alpha__("bWF4")
+_C=alpha__("bWF4")
+_B=alpha__("bWlu")
 _A=None
+from homeassistant.helpers.event import async_track_time_interval
+from collections.abc import Callable
+from datetime import timedelta
 import logging
 from typing import Any
 from TISControlProtocol.api import TISApi
@@ -20,15 +23,22 @@ from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from.import TISConfigEntry
 from.const import FAN_MODES,TEMPERATURE_RANGES
 handler=TISProtocolHandler()
+POLLING_INTERVAL=timedelta(seconds=60)
 async def async_setup_entry(hass,entry,async_add_devices):
     H=alpha__("Z2F0ZXdheQ==");G=alpha__("aXNfcHJvdGVjdGVk");F=alpha__("ZGV2aWNlX2lk");E=alpha__("Y2hhbm5lbHM=");B=async_add_devices;A=entry.runtime_data.api;C=await A.get_entities(platform=alpha__("YWM="))
     if C:I=[(C,next(iter(A[E][0].values())),A[F],A[G],A[H])for B in C for(C,A)in B.items()];J=[TISClimate(tis_api=A,ac_name=B,ac_number=C,device_id=D,gateway=E)for(B,C,D,F,E)in I];B(J)
     D=await A.get_entities(platform=alpha__("Zmxvb3JfaGVhdGluZw=="))
     if D:K=[(C,next(iter(A[E][0].values())),A[F],A[G],A[H])for B in D for(C,A)in B.items()];L=[TISFloorHeating(tis_api=A,heater_name=B,heater_number=C,device_id=D,gateway=E)for(B,C,D,F,E)in K];B(L)
 class TISClimate(ClimateEntity):
-    def __init__(A,tis_api,ac_name,ac_number,device_id,gateway):A.api=tis_api;A._name=ac_name;A.device_id=device_id;A.ac_number=int(ac_number)-1;A._attr_unique_id=beta__("YWNfe19fdmFyMH1fe19fdmFyMX0=", __var0=A.device_id, __var1=A.ac_number);A.gateway=gateway;A._attr_temperature_unit=UnitOfTemperature.CELSIUS;A._unit_index=0 if A._attr_temperature_unit==UnitOfTemperature.CELSIUS else 1;A.update_packet=handler.generate_ac_update_packet(A);A.listener=_A;A._attr_state=STATE_OFF;A._attr_target_temperature=_A;A._attr_max_temp=_A;A._attr_min_temp=_A;A._attr_target_temperature_step=_A;A.setup_ac()
-    def setup_ac(A):A._attr_state=STATE_UNKNOWN;A._attr_target_temperature=_A;A._attr_hvac_mode=HVACMode.OFF;A._attr_fan_mode=FAN_MEDIUM;A._attr_max_temp=TEMPERATURE_RANGES[A._attr_hvac_mode][_B][A._unit_index];A._attr_min_temp=TEMPERATURE_RANGES[A._attr_hvac_mode][_C][A._unit_index];A._attr_target_temperature_step=1 if A._unit_index==0 else 2;A._attr_hvac_modes=[HVACMode.OFF,HVACMode.HEAT,HVACMode.COOL,HVACMode.AUTO,HVACMode.FAN_ONLY];A._attr_supported_features=ClimateEntityFeature.FAN_MODE|ClimateEntityFeature.TARGET_TEMPERATURE|ClimateEntityFeature.TURN_OFF|ClimateEntityFeature.TURN_ON;A._attr_fan_modes=[FAN_AUTO,FAN_LOW,FAN_MEDIUM,FAN_HIGH];A.mode_target_temperatures={HVACMode.COOL:20,HVACMode.HEAT:30,HVACMode.FAN_ONLY:_A,HVACMode.AUTO:20,HVACMode.OFF:_A}
+    def __init__(A,tis_api,ac_name,ac_number,device_id,gateway):A.api=tis_api;A._name=ac_name;A.device_id=device_id;A.ac_number=int(ac_number)-1;A._attr_unique_id=beta__("YWNfe19fdmFyMH1fe19fdmFyMX0=", __var0=A.device_id, __var1=A.ac_number);A.gateway=gateway;A._attr_temperature_unit=UnitOfTemperature.CELSIUS;A._unit_index=0 if A._attr_temperature_unit==UnitOfTemperature.CELSIUS else 1;A.update_packet=handler.generate_ac_update_packet(A);A.listener=_A;A._attr_state=STATE_UNKNOWN;A._attr_target_temperature=_A;A._attr_max_temp=_A;A._attr_min_temp=_A;A._attr_target_temperature_step=_A;A.setup_ac()
+    def setup_ac(A):A._attr_state=STATE_UNKNOWN;A._attr_target_temperature=_A;A._attr_hvac_mode=_A;A._attr_fan_mode=FAN_MEDIUM;A._attr_max_temp=15;A._attr_min_temp=26;A._attr_target_temperature_step=1 if A._unit_index==0 else 2;A._attr_hvac_modes=[HVACMode.OFF,HVACMode.HEAT,HVACMode.COOL,HVACMode.AUTO,HVACMode.FAN_ONLY];A._attr_supported_features=ClimateEntityFeature.FAN_MODE|ClimateEntityFeature.TARGET_TEMPERATURE|ClimateEntityFeature.TURN_OFF|ClimateEntityFeature.TURN_ON;A._attr_fan_modes=[FAN_AUTO,FAN_LOW,FAN_MEDIUM,FAN_HIGH];A.mode_target_temperatures={HVACMode.COOL:20,HVACMode.HEAT:30,HVACMode.FAN_ONLY:_A,HVACMode.AUTO:20,HVACMode.OFF:_A};A._update_task_unsub=_A
+    def _start_polling(A):
+        if not A._update_task_unsub:logging.info(beta__("U3RhcnRpbmcgc3RhdGUgcG9sbGluZyBmb3Ige19fdmFyMH0=", __var0=A.name));A._update_task_unsub=async_track_time_interval(A.hass,A._async_poll_for_state,POLLING_INTERVAL)
+    def _stop_polling(A):
+        if A._update_task_unsub:logging.info(beta__("U3RvcHBpbmcgc3RhdGUgcG9sbGluZyBmb3Ige19fdmFyMH0=", __var0=A.name));A._update_task_unsub();A._update_task_unsub=_A
+    async def _async_poll_for_state(A,now=_A):logging.info(beta__("UG9sbGluZyBmb3Igc3RhdGUgb2Yge19fdmFyMH0=", __var0=A.name));await A.api.protocol.sender.send_packet(A.update_packet)
     async def async_added_to_hass(A):
+        A._start_polling()
         @callback
         async def B(event):
             F=alpha__("cGFja2V0X21vZGVfaW5kZXg=");B=event
@@ -52,12 +62,12 @@ class TISClimate(ClimateEntity):
                     if B.data[alpha__("YWNfbnVtYmVy")]==A.ac_number:
                         if B.data[alpha__("c3RhdGU=")]==0:A._attr_state=STATE_OFF;A._attr_hvac_mode=HVACMode.OFF
                         else:
-                            A._attr_state=STATE_ON;A._attr_hvac_mode=next((A for(A,C)in TEMPERATURE_RANGES.items()if C[F]==B.data[alpha__("aHZhY19tb2Rl")]),_A);A._attr_fan_mode=next(A for(A,C)in FAN_MODES.items()if C==B.data[alpha__("ZmFuX3NwZWVk")]);A._attr_min_temp=TEMPERATURE_RANGES[A.hvac_mode][_C][A._unit_index];A._attr_max_temp=TEMPERATURE_RANGES[A.hvac_mode][_B][A._unit_index]
+                            A._attr_state=STATE_ON;A._attr_hvac_mode=next((A for(A,C)in TEMPERATURE_RANGES.items()if C[F]==B.data[alpha__("aHZhY19tb2Rl")]),_A);A._attr_fan_mode=next(A for(A,C)in FAN_MODES.items()if C==B.data[alpha__("ZmFuX3NwZWVk")]);A._attr_min_temp=TEMPERATURE_RANGES[A.hvac_mode][_B][A._unit_index];A._attr_max_temp=TEMPERATURE_RANGES[A.hvac_mode][_C][A._unit_index]
                             if A._attr_hvac_mode==HVACMode.COOL:A._attr_target_temperature=B.data[alpha__("Y29vbF90ZW1w")]
                             elif A._attr_hvac_mode==HVACMode.HEAT:A._attr_target_temperature=B.data[alpha__("aGVhdF90ZW1w")]
                             elif A._attr_hvac_mode==HVACMode.AUTO:A._attr_target_temperature=B.data[alpha__("YXV0b190ZW1w")]
                             else:A._attr_target_temperature=_A
-            A.async_write_ha_state();await A.async_update_ha_state(_D)
+            A._stop_polling();A.async_write_ha_state();await A.async_update_ha_state(_D)
         A.listener=A.hass.bus.async_listen(str(A.device_id),B);await A.api.protocol.sender.send_packet(A.update_packet)
     @property
     def name(self):return self._name
@@ -81,24 +91,24 @@ class TISClimate(ClimateEntity):
     async def async_set_hvac_mode(A,hvac_mode):
         B=hvac_mode
         if B==HVACMode.OFF:C=STATE_OFF;D=_A;E=_A;F=_A
-        else:C=STATE_ON;E=TEMPERATURE_RANGES[B][_C][A._unit_index];F=TEMPERATURE_RANGES[B][_B][A._unit_index];D=A.mode_target_temperatures[B]
+        else:C=STATE_ON;E=TEMPERATURE_RANGES[B][_B][A._unit_index];F=TEMPERATURE_RANGES[B][_C][A._unit_index];D=A.mode_target_temperatures[B]
         G=handler.generate_ac_control_packet(A,TEMPERATURE_RANGES,FAN_MODES,target_state=C,target_temperature=D,target_mode=B);H=await A.api.protocol.sender.send_packet_with_ack(G)
-        if H:A._attr_hvac_mode=B;A._attr_state=C;A._attr_min_temp=E;A._attr_max_temp=F;A._attr_target_temperature=D
-        else:logging.error(alpha__("RmFpbGVkIHRvIHNldCBodmFjIG1vZGU="));A._attr_state=STATE_UNKNOWN;A._attr_hvac_mode=_A
+        if H:A._attr_hvac_mode=B;A._attr_state=C;A._attr_min_temp=E;A._attr_max_temp=F;A._attr_target_temperature=D;A._stop_polling()
+        else:logging.error(alpha__("RmFpbGVkIHRvIHNldCBodmFjIG1vZGU="));A._attr_state=STATE_UNKNOWN;A._start_polling();A._attr_hvac_mode=_A
         A.async_write_ha_state()
     async def async_set_fan_mode(A,fan_mode):
         B=fan_mode;C=handler.generate_ac_control_packet(A,TEMPERATURE_RANGES,FAN_MODES,target_fan_mode=B);D=await A.api.protocol.sender.send_packet_with_ack(C)
-        if D:A._attr_fan_mode=B
-        else:logging.error(alpha__("RmFpbGVkIHRvIHNldCBmYW4gbW9kZQ=="));A._attr_state=STATE_UNKNOWN;A._attr_fan_mode=_A
+        if D:A._attr_fan_mode=B;A._stop_polling()
+        else:logging.error(alpha__("RmFpbGVkIHRvIHNldCBmYW4gbW9kZQ=="));A._attr_state=STATE_UNKNOWN;A._start_polling();A._attr_fan_mode=_A
         A.async_write_ha_state()
     async def async_set_temperature(A,**C):
         B=C.get(ATTR_TEMPERATURE);D=handler.generate_ac_control_packet(A,TEMPERATURE_RANGES,FAN_MODES,target_temperature=B);E=await A.api.protocol.sender.send_packet_with_ack(D)
-        if E:A._attr_target_temperature=B;A.mode_target_temperatures[A.hvac_mode]=B if B else A.target_temperature
-        else:A._attr_state=STATE_UNKNOWN;logging.error(alpha__("RmFpbGVkIHRvIHNldCB0ZW1wZXJhdHVyZQ=="));A._attr_target_temperature=_A;A._attr_hvac_mode=_A
+        if E:A._attr_target_temperature=B;A._stop_polling();A.mode_target_temperatures[A.hvac_mode]=B if B else A.target_temperature
+        else:A._attr_state=STATE_UNKNOWN;logging.error(alpha__("RmFpbGVkIHRvIHNldCB0ZW1wZXJhdHVyZQ=="));A._attr_target_temperature=_A;A._attr_hvac_mode=_A;A._start_polling()
         A.async_write_ha_state()
 class TISFloorHeating(ClimateEntity):
     def __init__(A,tis_api,heater_name,heater_number,device_id,gateway):A.api=tis_api;A._name=heater_name;A.device_id=device_id;A.heater_number=int(heater_number)-1;A._attr_unique_id=beta__("Zmxvb3JfaGVhdGVyX3tfX3ZhcjB9X3tfX3ZhcjF9", __var0=A.device_id, __var1=A.heater_number);A.gateway=gateway;A._attr_temperature_unit=UnitOfTemperature.CELSIUS;A._unit_index=0 if A._attr_temperature_unit==UnitOfTemperature.CELSIUS else 1;A.update_packet=handler.generate_floor_update_packet(A);A.listener=_A;A._attr_state=STATE_OFF;A._attr_target_temperature=_A;A._attr_current_temperature=_A;A._attr_max_temp=_A;A._attr_min_temp=_A;A._attr_target_temperature_step=_A;A.setup_heater()
-    def setup_heater(A):A._attr_hvac_mode=HVACMode.HEAT;A._attr_max_temp=TEMPERATURE_RANGES[A._attr_hvac_mode][_B][A._unit_index];A._attr_min_temp=TEMPERATURE_RANGES[A._attr_hvac_mode][_C][A._unit_index];A._attr_target_temperature=TEMPERATURE_RANGES[A._attr_hvac_mode][alpha__("dGFyZ2V0")][A._unit_index];A._attr_target_temperature_step=1 if A._unit_index==0 else 2;A._attr_hvac_modes=[HVACMode.OFF,HVACMode.HEAT];A._attr_supported_features=ClimateEntityFeature.TARGET_TEMPERATURE|ClimateEntityFeature.TURN_OFF|ClimateEntityFeature.TURN_ON;A.mode_target_temperatures={HVACMode.HEAT:30,HVACMode.OFF:_A}
+    def setup_heater(A):A._attr_hvac_mode=HVACMode.HEAT;A._attr_max_temp=TEMPERATURE_RANGES[A._attr_hvac_mode][_C][A._unit_index];A._attr_min_temp=TEMPERATURE_RANGES[A._attr_hvac_mode][_B][A._unit_index];A._attr_target_temperature=TEMPERATURE_RANGES[A._attr_hvac_mode][alpha__("dGFyZ2V0")][A._unit_index];A._attr_target_temperature_step=1 if A._unit_index==0 else 2;A._attr_hvac_modes=[HVACMode.OFF,HVACMode.HEAT];A._attr_supported_features=ClimateEntityFeature.TARGET_TEMPERATURE|ClimateEntityFeature.TURN_OFF|ClimateEntityFeature.TURN_ON;A.mode_target_temperatures={HVACMode.HEAT:30,HVACMode.OFF:_A}
     async def async_added_to_hass(A):
         @callback
         async def B(event):
@@ -118,7 +128,7 @@ class TISFloorHeating(ClimateEntity):
                     if B.data[alpha__("aGVhdGVyX251bWJlcg==")]==A.heater_number:
                         if B.data[alpha__("c3RhdGU=")]==0:A._attr_state=STATE_OFF;A._attr_hvac_mode=HVACMode.OFF
                         else:
-                            A._attr_state=STATE_ON;A._attr_hvac_mode=HVACMode.HEAT;A._attr_min_temp=TEMPERATURE_RANGES[A.hvac_mode][_C][A._unit_index];A._attr_max_temp=TEMPERATURE_RANGES[A.hvac_mode][_B][A._unit_index]
+                            A._attr_state=STATE_ON;A._attr_hvac_mode=HVACMode.HEAT;A._attr_min_temp=TEMPERATURE_RANGES[A.hvac_mode][_B][A._unit_index];A._attr_max_temp=TEMPERATURE_RANGES[A.hvac_mode][_C][A._unit_index]
                             if A._attr_hvac_mode==HVACMode.HEAT:A._attr_target_temperature=B.data[alpha__("dGVtcA==")]
                             else:A._attr_target_temperature=_A
             A.async_write_ha_state();await A.async_update_ha_state(_D)
