f263abcb1da29b0f40c48adec4186527495f5dce
Tis-Control-HA <tishomeassistant@gmail.com>
2026-01-13 21:43:26 +0200

    new version

--- binary_sensor.py
+++ binary_sensor.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 from TISControlProtocol import *
+_D=alpha__("cGFzc3dvcmQ=")
 _C=False
 _B=True
 _A=None
@@ -11,13 +12,12 @@ from homeassistant.core import Event,HomeAssistant,callback
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.event import async_call_later
 from.import TISConfigEntry
-import logging
 async def async_setup_entry(hass,entry,async_add_entities):
-    A=entry.runtime_data.api;D=await A.get_entities(platform=alpha__("YmluYXJ5X3NlbnNvcg=="));E=await A.get_entities(platform=alpha__("cGFzc3dvcmRz"));B=[]
-    if D:
-        C=[(C,next(iter(A[alpha__("Y2hhbm5lbHM=")][0].values())),A[alpha__("ZGV2aWNlX2lk")],A[alpha__("Z2F0ZXdheQ==")],A[alpha__("aXNfcHJvdGVjdGVk")])for B in D for(C,A)in B.items()]
-        for(F,G,H,I,J)in C:B.append(TISBinarySensor(tis_api=A,sensor_name=F,channel_number=G,device_id=H,gateway=I))
-    if E:C=[(B,C)for A in E for(B,C)in A.items()];B.append(TISPasswordSensor(tis_api=A,password_name=B,password=C)for(B,C)in C)
+    A=entry.runtime_data.api;C=await A.get_entities(platform=alpha__("YmluYXJ5X3NlbnNvcg=="));D=await A.get_entities(platform=alpha__("cGFzc3dvcmRz"));B=[]
+    if C:
+        E=[(C,next(iter(A[alpha__("Y2hhbm5lbHM=")][0].values())),A[alpha__("ZGV2aWNlX2lk")],A[alpha__("Z2F0ZXdheQ==")],A[alpha__("aXNfcHJvdGVjdGVk")])for B in C for(C,A)in B.items()]
+        for(F,G,H,I,J)in E:B.append(TISBinarySensor(tis_api=A,sensor_name=F,channel_number=G,device_id=H,gateway=I))
+    if D:B.extend(TISPasswordSensor(tis_api=A,password_name=B.get(alpha__("dGl0bGU=")),password=B.get(_D))for B in D)
     async_add_entities(B)
 class TISBinarySensor(BinarySensorEntity):
     def __init__(A,tis_api,sensor_name,channel_number,device_id,gateway):A._api=tis_api;A._name=sensor_name;A._device_id=device_id;A._channel_number=int(channel_number);A._listener=_A;A._attr_state=_A;A._attr_is_on=_A;A._attr_device_class=alpha__("bW90aW9u"),;A._gateway=gateway;A._attr_unique_id=beta__("e19fdmFyMH1fe19fdmFyMX0=", __var0=A._name, __var1=A._channel_number)
@@ -49,7 +49,7 @@ class TISPasswordSensor(BinarySensorEntity):
         async def B(event):
             B=event
             if B.event_type==alpha__("cGFzc3dvcmRfZmVlZGJhY2s="):
-                if B.data[alpha__("cGFzc3dvcmQ=")]==A._password:A._attr_is_on=_B;A.async_write_ha_state();async_call_later(A.hass,1,A._turn_off_callback)
+                if B.data[_D]==A._password:A._attr_is_on=_B;A.async_write_ha_state();async_call_later(A.hass,1,A._turn_off_callback)
         A._listener=A.hass.bus.async_listen(MATCH_ALL,B)
     @callback
     async def _turn_off_callback(self,_now):self._attr_is_on=_C;await self.async_write_ha_state()
